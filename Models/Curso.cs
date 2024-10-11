c3f0e685159fa7e2b9763ec937e63ad057fc2d20
fabricio rosa <fabriciorosafiap@gmail.com>
2024-10-11 19:50:00 -0300

    Add project files.

---
+++ Models/Curso.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManipulandoValores.Models
+{
+    public class Curso
+    {
+        public string Name { get; set; } //criando uma prop Name 
+        public List<Pessoa> Student { get; set; } //criando uma prop Pessoa do tipo List 
+
+        public void AddStudent(Pessoa student)
+        {
+            Student.Add(student);
+        }
+        //um metodo diferente de void, precisa ter um return      
+        public int GetQuantityOfStudentsEnrolled()
+        {
+            int quantity = Student.Count;
+            return quantity;
+        }
+
+        public bool RemoveStudent(Pessoa student) //todo metodo diferente de VOID precisa de um RETURN no code
+        {
+            Student.Remove(student);
+            return true;
+        }
+
+        public void ListStudent()
+        {
+            Console.WriteLine($"Alunos do curso de: {Name}");
+
+
+            for (int count = 0; count < Student.Count; count++)
+            {
+                //concatenando string
+                //string text = "Numero: " + count + "  -  " + Student[count].Fullname; // concatenação
+                string text = $"Número: {count} - {Student[count].Fullname}"; //melhor formato interpolação
+                Console.WriteLine(text);
+            }
+
+        }
+
+    }
+}
+    
+
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using ManipulandoValores.Models;
+using System.Globalization;
+
+
+Pessoa pessoa1 = new Pessoa(name: "Fabricio", lastname: "Rosa");
+Pessoa pessoa2 = new Pessoa(name: "Bianca", lastname: "Silva");
+
+
+Curso cursoOfIngles = new Curso();
+cursoOfIngles.Name = "Ingles";
+cursoOfIngles.Student = new List<Pessoa>();
+
+cursoOfIngles.AddStudent(pessoa1);
+cursoOfIngles.AddStudent(pessoa2);
+cursoOfIngles.ListStudent();
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+
+decimal valorMonetario = 145672320.00M;
+
+Console.WriteLine($"{valorMonetario:C}");
+
+double porcetagem = .3421;
+
+Console.WriteLine(porcetagem.ToString("P"));
+
+int numero = 543210;
+
+Console.WriteLine(numero.ToString("##-##-##"));
+
+DateTime data = DateTime.Now;
+DateTime data1 = DateTime.Parse("11/11/1970 15:20");
+
+Console.WriteLine(data.ToString("dd/MM/yyyy HH:mm"));
+
+Console.WriteLine(data.ToShortTimeString());
+
+Console.WriteLine(data.ToShortDateString());
+
+
+string dataString = "17-04-2024 17:00";
+
+DateTime.TryParseExact(dataString, "dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime data2);
+
+//DateTime data2 = DateTime.Parse (dataString);
+Console.WriteLine(data2);
